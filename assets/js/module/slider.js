1effa3d12729be723debec5f0a090580249db057
djamila key <sansondjamila@orange.fr>
2023-10-16 16:13:58 +0200

    sv

--- assets/js/module/slider.js
+++ assets/js/module/slider.js
@@ -1,19 +1,36 @@
-const slider = (catalogue,currentTrack) => {
-
+const slider = (catalogue, currentTrack, status = "init") => {
     console.log("initialisation du slider");
-    // console.dir(catalogue);
-
-    // console.img(catalogue[0] .cover);
-    const sliderhtml = document.querySelector("#slider");
+    /* console.dir(catalogue);
+    console.log(catalogue[0].cover); */
+    //console.log(catalogue[0]["cover"]);
+    const sliderHTML = document.querySelector("#slider");
     const coverUrl = "./assets/img/cover/";
 
 
-    const coverSlider = document.createElement("img");
-    coverSlider.src = coverUrl + catalogue[currentTrack].cover;
-    sliderhtml.append(coverSlider);
+    switch (status) {
+        case "init":
+            const coverSlider = document.createElement("img");
+            coverSlider.src = coverUrl + catalogue[currentTrack].cover;
+            coverSlider.id = "coverSlider";
+            // prepend insere un element avant ceux qui existent déjà dans le parent
+            //sliderHTML.prepend(coverSlider);
+            // append insere un element après ceux qui existent déjà dans le parent
+            sliderHTML.append(coverSlider);
+            break;
+        case "next":
+            document.querySelector("#coverSlider").src =
+                coverUrl + catalogue[currentTrack].cover;
+            break;
+        case "prev":
+            document.querySelector("#coverSlider").src =
+                coverUrl + catalogue[currentTrack].cover;
+            break;
+        default:
+            break;
+    }
 
-   
-    // sliderhtml.prepend(coverSlider);
+
+
 };
 
 export { slider };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,28 @@
 import { catalogue } from "./module/catalogue.js";
 import { slider } from "./module/slider.js";
-
-
-console.dir(catalogue);
+//console.dir(catalogue);
 let currentTrack = 0;
-const prevButton = document.querySelector("#prev")
-const nextButton = document.querySelector("#next")
-nextButton.addEventListener("click", () => {
-
-    if (currentTrack < catalogue.length - 1) {
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+// click sur le bouton next
+nextButton.addEventListener("click",()=>{
+    if (currentTrack < catalogue.length-1) {
         currentTrack++;
     } else {
         currentTrack = 0;
-
     }
-
-
-
+    slider(catalogue,currentTrack,"next");
     console.log(currentTrack);
-});
-
-prevButton.addEventListener("click", () => {
-
+})
+// idem pour previous
+prevButton.addEventListener("click",()=>{
     if (currentTrack > 0) {
         currentTrack--;
     } else {
-        currentTrack = catalogue.length - 1;
-
+        currentTrack = catalogue.length-1;
     }
-
-
-
+    slider(catalogue,currentTrack,"prev");
     console.log(currentTrack);
-});
+})
 
-slider(catalogue, currentTrack); 
+slider(catalogue,currentTrack);
