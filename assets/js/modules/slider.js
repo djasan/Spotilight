9deda8f91cc4c05b1001b80b1fa571d36893feeb
djamila key <sansondjamila@orange.fr>
2023-10-17 15:32:17 +0200

    sv

--- assets/js/modules/slider.js
+++ assets/js/modules/slider.js
@@ -1,21 +1,33 @@
-const slider = ( status = "init") => {
+
+const sliderHTML = document.querySelector("#slider");
+const coverUrl = "./assets/img/cover/";
+
+const initSlider = ()=>{
+const coverSlider = document.createElement("img");
+coverSlider.src = coverUrl + catalogue[currentTrack].cover;
+coverSlider.id = "coverSlider";
+// prepend insere un element avant ceux qui existent déjà dans le parent
+//sliderHTML.prepend(coverSlider);
+// append insere un element après ceux qui existent déjà dans le parent
+sliderHTML.append(coverSlider);
+
+const imgA =document.createElement("img");
+imgA.id= "imgA";
+sliderHTML.append(imgA);
+}
+
+const slider = (status = "init") => {
     console.log("initialisation du slider");
     /* console.dir(catalogue);
     console.log(catalogue[0].cover); */
     //console.log(catalogue[0]["cover"]);
-    const sliderHTML = document.querySelector("#slider");
-    const coverUrl = "./assets/img/cover/";
+   
 
 
     switch (status) {
         case "init":
-            const coverSlider = document.createElement("img");
-            coverSlider.src = coverUrl +catalogue[currentTrack].cover;
-            coverSlider.id = "coverSlider";
-            // prepend insere un element avant ceux qui existent déjà dans le parent
-            //sliderHTML.prepend(coverSlider);
-            // append insere un element après ceux qui existent déjà dans le parent
-            sliderHTML.append(coverSlider);
+            initSlider();
+
             break;
         case "next":
             document.querySelector("#coverSlider").src =
